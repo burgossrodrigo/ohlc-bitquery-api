c9b9861b81014b66f6060324ed0f0cce5c1cf14d
burgossrodrigo <rodrigoburgos@live.com>
2022-04-05 20:59:28 -0300

    ref: more cors refactory

--- index.js
+++ index.js
@@ -16,25 +16,8 @@ app.listen(PORT, server_host, () => {
     console.log(`server is listening on port: ${PORT}`)
 })
 
-app.use(cors({
-  origin: 'https://hokkfi-ohlc-api.herokuapp.com/'
-}));
+app.use(cors());
 
-var allowedOrigins = ['http://localhost:3000/#/swap',
-                      'https://hokkfi-ohlc-api.herokuapp.com/'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
 
 router.get('/:chain/:address/:from/:to/:resolution', [], async (req, res) => {
 

7477e87669a98708d6213ade8532d784cf88bc29
burgossrodrigo <rodrigoburgos@live.com>
2022-04-05 20:08:57 -0300

    feat: cors

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ app.listen(PORT, server_host, () => {
     console.log(`server is listening on port: ${PORT}`)
 })
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 router.get('/:chain/:address/:from/:to/:resolution', [], async (req, res) => {
 
     const chain = req.query.chain;

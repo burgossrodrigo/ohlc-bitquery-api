ff9b599c2cc6aef17fd684fd6ddf9149cf5a07f4
burgossrodrigo <rodrigoburgos@live.com>
2022-04-06 03:48:01 -0300

    cors running

--- index.js
+++ index.js
@@ -18,186 +18,78 @@ app.listen(PORT, server_host, () => {
 
 app.use(cors());
 
+const corsOptions ={
+  origin: false
+}
 
-router.get('/:chain/:address/:from/:to/:resolution', [], async (req, res) => {
 
-    const chain = req.query.chain;
+router.get('/:network/:address/:from/:to/:resolution', cors(corsOptions), async (req, res) => {
 
-    const baseAddress = req.params.address;
+     const network = req.params.network;
 
-    const factoryAddress = () => {
-      if(chain === 'ethereum'){
-        return '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
-      }
+     const baseAddress = req.params.address;
 
-      if(chain === 'bsc'){
-        return '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
-      }
-    };
+    const factoryAddress = network === 'ethereum' ? '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f' : '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
 
-    const quoteAddress = () => {
-      if(chain === 'ethereum'){
-        return '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-      }
-      if(chain === 'bsc'){
-        return '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-      }
-    };
+
+    const quoteAddress = network === 'ethereum' ? '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' : '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c';
 
     const from = req.params.from;
 
     const to = req.params.to;
 
     const resolution = req.params.resolution;
+
+    console.log('factory: ' + factoryAddress)
+    console.log('ether: ' + quoteAddress)
+
+    try{
     
-    let changedData = []
-    await fetch(endpoint, {
+    await fetch('https://graphql.bitquery.io', {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
         "X-API-KEY": "BQYpMaOs1jlSOr3CQdT0yzQMdld6r8Mn"
       },
-      mode: 'no-cors',
       body: JSON.stringify({
         query: `
-            {
-              ethereum(network: ${chain}) {
-                dexTrades(
-                  date: {between: ["${from}", "${to}"]}
-                  exchangeAddress: {is: "${factoryAddress}"}
-                  baseCurrency: {is: "${baseAddress}"}
-                  quoteCurrency: {is: "${quoteAddress}"}
-                ) 
-                {
-                  timeInterval {
-                    minute(count: ${resolution}, format: "%Y-%m-%dT%H:%M:%SZ")  
-                  }
-                  volume: quoteAmount
-                  high: quotePrice(calculate: maximum)
-                  low: quotePrice(calculate: minimum)
-                  open: minimum(of: block, get: quote_price)
-                  close: maximum(of: block, get: quote_price) 
-                }
-              }
-            }
-          `})
-    })
-      .then((res) => res.json())
-      .then(res => res.data.ethereum.dexTrades.map(chart =>{
-        
-        /*
-        var myDate = chart.timeInterval;
-        myDate = myDate.split("-");
-        var newDate = new Date( myDate[2], myDate[1] - 1, myDate[0]);
-        console.log(newDate.getTime());
-        */
-
-    changedData.push({
-      time: Number(moment(chart.timeInterval.day).format('X')),
-      low: chart.minimum_price,
-      high: chart.maximum_price,
-      open: Number(chart.open_price),
-      close: Number(chart.close_price),
-      volume: chart.trades
-    });
-
-    }));
-
-    return res.json(changedData);
-
-
-})
-
-router.get('/hokk/:chain/:from/:to', [], async (req, res) => {
-  const chain = req.query.chain;
-  const factoryAddress = () => {
-    if(chain === 'ethereum'){
-      return '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
-    }
-
-    if(chain === 'bsc'){
-      return '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
-    }
-  };
-
-  const quoteAddress = () => {
-    if(chain === 'ethereum'){
-      return '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    }
-    if(chain === 'bsc'){
-      return '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-    }
-  };
-
-  
-
-  let changedData = []
-    await fetch('https://graphql.bitquery.io', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', "X-API-KEY": "BQYTYsxZMZA47wBr1PvuU8jYWieM3HSd" },
-        body: JSON.stringify({ query: `
         {
-          ethereum(network: bsc) {
+          ethereum(network: ${network}) {
             dexTrades(
-              date: {between: ["${from}", "${to}"]}
+              options: {asc: "timeInterval.minute"}
+              date: {since: "${from}", till: "${to}"}
               exchangeAddress: {is: "${factoryAddress}"}
-              baseCurrency: {is: "0xe87e15b9c7d989474cb6d8c56b3db4efad5b21e8"}
+              baseCurrency: {is: "${baseAddress}"}
               quoteCurrency: {is: "${quoteAddress}"}
-            ) {
-              exchange {
-                name
-              }
+              tradeAmountUsd: {gt: 10}
+            ) 
+            {
               timeInterval {
-                day(count: 1)
+                minute(count: ${resolution}, format: "%Y-%m-%dT%H:%M:%SZ")  
               }
-              baseCurrency {
-                symbol
-                address
-              }
-              baseAmount
-              quoteCurrency {
-                symbol
-                address
-              }
-              quoteAmount
-              trades: count
-              maximum_price: quotePrice(calculate: maximum)
-              minimum_price: quotePrice(calculate: minimum)
-              open_price: minimum(of: time, get: quote_price)
-              close_price: maximum(of: time, get: quote_price)
-              tradeAmount(in: USD, calculate: sum)
+              volume: quoteAmount
+              high: quotePrice(calculate: maximum)
+              low: quotePrice(calculate: minimum)
+              open: minimum(of: block, get: quote_price)
+              close: maximum(of: block, get: quote_price) 
             }
           }
-        }` 
-        }),
-      })
-      .then(res => res.json())
-      .then(res => res.data.ethereum.dexTrades.map(chart =>{
+        }`})
+    })
+      .then((res) => res.json())
+      .then(res => 
+        {
+          console.log(res)
+          return res
         
-        /*
-        var myDate = chart.timeInterval;
-        myDate = myDate.split("-");
-        var newDate = new Date( myDate[2], myDate[1] - 1, myDate[0]);
-        console.log(newDate.getTime());
-        */
+        })
 
-    changedData.push({
-      time: Number(moment(chart.timeInterval.day).format('X')),
-      low: chart.minimum_price,
-      high: chart.maximum_price,
-      open: Number(chart.open_price),
-      close: Number(chart.close_price),
-      volume: chart.trades
-    });
-
-    }));
-
-    console.log(changedData);
-    return res.json(changedData);
-
-
-
+}catch(err){
+  console.log(err);
+}
 })
+
+
 
 
 

97b374813cb76c28a8905153a3d9bc90406ec147
John Wilhelmsson <81255184+jwtradera@users.noreply.github.com>
2022-04-07 22:36:34 +0700

    Update stages

--- index.js
+++ index.js
@@ -1,61 +1,98 @@
 const express = require('express')
 const bodyParse = require('body-parser')
-require('isomorphic-fetch');
-var cors = require('cors');
+const axios = require('axios')
+const cors = require('cors');
+require('dotenv').config();
 
-const json = bodyParse.json
+
+const PORT = process.env.PORT || 5000
+const HOST_ADDRESS = process.env.HOST_ADDRESS || '0.0.0.0';
+const BITQUERY_KEY = process.env.BITQUERY_KEY || '';
+const BITQUERY_URL = 'https://graphql.bitquery.io';
+
 const app = express();
+
 const router = express.Router()
 
-app.use(json());
+app.use(bodyParse.json());
 app.use(router);
-const PORT = process.env.PORT || 5000
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(PORT, server_host, () => {
-    console.log(`server is listening on port: ${PORT}`)
+app.listen(PORT, HOST_ADDRESS, () => {
+  console.log(`server is listening on port: ${PORT}`)
 })
 
 app.use(cors());
 
-const corsOptions ={
+const corsOptions = {
   origin: false
 }
 
+router.get('/symbol_info/:network/:address', cors(corsOptions), async (req, res) => {
 
-router.get('/:network/:address/:from/:to/:resolution', cors(corsOptions), async (req, res) => {
+  try {
 
-     const network = req.params.network;
+    const network = req.params.network;
+    const address = req.params.address;
+    console.log(network, address);
 
-     const baseAddress = req.params.address;
+    const { data } = await axios.post(BITQUERY_URL, JSON.stringify({
+      query: `
+      {
+        ethereum(network: ${network}) {
+          address(address: {is: "${address}"}) {
+            smartContract {
+              currency {
+                symbol
+                name
+                decimals
+                tokenType
+              }
+            }
+          }
+        }
+      }`}),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": BITQUERY_KEY
+        }
+      });
 
-    const factoryAddress = network === 'ethereum' ? '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f' : '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+    const result = data['data']['ethereum']['address'][0]['smartContract']['currency'];
+    return res.json(result);
 
+  } catch (err) {
+    console.log(err);
+  }
 
-    const quoteAddress = network === 'ethereum' ? '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' : '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c';
+})
 
-    const from = req.params.from;
+router.get('/ohlc_data/:network/:address/:from/:to/:resolution', cors(corsOptions), async (req, res) => {
 
-    const to = req.params.to;
+  const network = req.params.network;
 
-    const resolution = req.params.resolution;
+  const baseAddress = req.params.address;
+  const factoryAddress = network === 'ethereum' ? '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f' : '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+  const quoteAddress = network === 'ethereum' ? '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' : '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c';
 
+  const from = req.params.from;
+  const to = req.params.to;
+  const resolution = req.params.resolution;
+
+  console.log(network, baseAddress, from, to, resolution);
+
+<<<<<<< HEAD
+  try {
+=======
     console.log('factory: ' + factoryAddress)
     console.log('ether: ' + quoteAddress)
     console.log('base currency: ' + baseAddress)
     console.log('from:' + from + ' to:' + to)
     console.log('network ' + network + ' and resolution: ' + resolution)
+>>>>>>> a547fdda85935eaa5dce3314e6b9df68345f6838
 
-    try{
-    
-    await fetch('https://graphql.bitquery.io', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        "X-API-KEY": "BQYpMaOs1jlSOr3CQdT0yzQMdld6r8Mn"
-      },
-      body: JSON.stringify({
-        query: `
+    const { data } = await axios.post(BITQUERY_URL, JSON.stringify({
+      query: `
         {
           ethereum(network: ${network}) {
             dexTrades(
@@ -63,7 +100,6 @@ router.get('/:network/:address/:from/:to/:resolution', cors(corsOptions), async 
               exchangeAddress: {is: "${factoryAddress}"}
               baseCurrency: {is: "${baseAddress}"}
               quoteCurrency: {is: "${quoteAddress}"}
-              tradeAmountUsd: {gt: 10}
             ) 
             {
               timeInterval {
@@ -77,58 +113,17 @@ router.get('/:network/:address/:from/:to/:resolution', cors(corsOptions), async 
             }
           }
         }`})
-    })
-      .then((res) => res.json())
-      .then(res => 
-        {
-          console.log(res)
-          return res
-        
-        })
+      , {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": BITQUERY_KEY
+        }
+      });
 
-}catch(err){
-  console.log(err);
-}
+    const result = data['data']['ethereum']['dexTrades'];
+    return res.json(result);
+
+  } catch (err) {
+    console.log(err);
+  }
 })
-
-
-
-
-
-/*
-
- const CHART_DATA =  gql`
-  query chart{
-ethereum(network: bsc) {
-dexTrades(
-options: {limit: 1000, asc: "timeInterval.minute"}
-date: {since: "2021-10-29"}
-exchangeName: {in:["Pancake","Pancake v2"]}
-baseCurrency: {is: "0xe87e15b9c7d989474cb6d8c56b3db4efad5b21e8"}
-quoteCurrency: {is: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"}p
-) {
-timeInterval {
-  minute(count: 5)
-}
-baseCurrency {
-  symbol
-  address
-}
-baseAmount
-quoteCurrency {
-  symbol
-  address
-}
-quoteAmount
-trades: count
-quotePrice
-maximum_price: quotePrice(calculate: maximum)
-minimum_price: quotePrice(calculate: minimum)
-open_price: minimum(of: block, get: quote_price)
-close_price: maximum(of: block, get: quote_price)
-}
-}
-}
-`;
-
-*/
